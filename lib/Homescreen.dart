83970dc9e150244d37fe07682c7c8d26de52770f
Hifza <hifzakanwal75@gmail.com>
2022-09-10 05:57:18 -0700

    Get_api_example_2

--- lib/Homescreen.dart
+++ lib/Homescreen.dart
@@ -1,10 +1,7 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
-
 import 'package:http/http.dart' as http;
-
-import 'Model.dart';
 
 class homescreen extends StatefulWidget {
   const homescreen({Key? key}) : super(key: key);
@@ -14,18 +11,20 @@ class homescreen extends StatefulWidget {
 }
 
 class _homescreenState extends State<homescreen> {
-  List<Getapimodel> modellist = [];
+  List<model> modellist = [];
 
-//future function, waiting for response from server
-
-  Future<List<Getapimodel>> getApiFun() async {
-    final response =
-        await http.get(Uri.parse("https://jsonplaceholder.typicode.com/posts"));
-    var data = jsonDecode(response.body.toString());
+  Future<List<model>> modelfun() async {
+    final response = await http
+        .get(Uri.parse("https://jsonplaceholder.typicode.com/photos"));
+    final data = jsonDecode(response.body.toString());
 
     if (response.statusCode == 200) {
+      // for (Map i in data) {
+      //modellist.add(Getapimodel.fromJson(i as Map<String, dynamic>));
+
       for (Map i in data) {
-        modellist.add(Getapimodel.fromJson(i as Map<String, dynamic>));
+        model modelg = model(title: i['title'], url: i['url'], id: i['id']);
+        modellist.add(modelg);
       }
       return modellist;
     } else {
@@ -36,52 +35,37 @@ class _homescreenState extends State<homescreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
       body: Column(
         children: [
-          //future builder because waiting for response
           Expanded(
             child: FutureBuilder(
-                future: getApiFun(),
-                builder: (context, snapshot) {
-                  if (!snapshot.hasData) {
-                    return Center(child: CircularProgressIndicator());
-                  } else {
-                    return ListView.builder(
-                        itemCount: modellist.length,
-                        itemBuilder: (context, index) {
-                          return Card(
-                            child: Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: Column(
-                                mainAxisAlignment: MainAxisAlignment.start,
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Text(modellist[index].id.toString()),
-                                  Text(
-                                    "Title",
-                                    style: TextStyle(
-                                        fontSize: 15,
-                                        fontWeight: FontWeight.bold),
-                                  ),
-                                  Text(modellist[index].title.toString()),
-                                  Text(
-                                    "Description",
-                                    style: TextStyle(
-                                        fontSize: 14,
-                                        fontWeight: FontWeight.bold),
-                                  ),
-                                  Text(modellist[index].body.toString()),
-                                ],
-                              ),
-                            ),
-                          );
-                        });
-                  }
+                future: modelfun(),
+                //list
+                builder: (context, AsyncSnapshot<List<model>> snapshot) {
+                  return ListView.builder(
+                      itemCount: modellist.length,
+                      itemBuilder: (context, index) {
+                        // Text(modellist[index].id.toString()),
+                        return ListTile(
+                          title: Text(snapshot.data![index].id.toString()),
+                          leading: CircleAvatar(
+                            backgroundImage: NetworkImage(
+                                snapshot.data![index].url.toString()),
+                          ),
+                          subtitle:
+                              Text(snapshot.data![index].title.toString()),
+                        );
+                      });
                 }),
           )
         ],
       ),
     );
   }
+}
+
+class model {
+  String? title, url;
+  int? id;
+  model({this.title, this.url, this.id});
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-
-import 'Homescreen.dart';
+import 'homescreen.dart';
 
 void main() {
   runApp(const MyApp());
